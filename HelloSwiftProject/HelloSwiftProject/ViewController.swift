2a0ac801f23e0895d249c4ad0bff6db1616a2c7a
오유진 <102959835+0hyujin@users.noreply.github.com>
2024-01-23 17:18:15 +0900

    Feat : Outlet, Action

--- HelloSwiftProject/HelloSwiftProject/ViewController.swift
+++ HelloSwiftProject/HelloSwiftProject/ViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func showValue(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
