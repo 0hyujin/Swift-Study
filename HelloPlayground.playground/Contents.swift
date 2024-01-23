1653007fa6edb51b2ac9ca72759580f0fd0aabb3
오유진 <102959835+0hyujin@users.noreply.github.com>
2024-01-23 16:16:15 +0900

    Feat : 출력, 콘솔

--- HelloPlayground.playground/Contents.swift
+++ HelloPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Hello, Swift!"
+
+print(greeting)
+print(greeting)
+
+UIImage(systemName : "star")
+
+
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+print(sum)
